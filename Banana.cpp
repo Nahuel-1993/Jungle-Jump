99870ec74d984e21a06c1b8756178803a95cab6c
Nahuel-1993 <nahuelfeijoo1993@gmail.com>
2024-11-02 18:06:17 -0300

    Se agrego clase Fruta para que hereden las demas frutas

--- Banana.cpp
+++ Banana.cpp
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "Banana.h"
+#include <stdlib.h>
 
 Banana::Banana() {
     _texture.loadFromFile("banana.png");
@@ -9,20 +9,12 @@ Banana::Banana() {
 }
 
 void Banana::update() {
-    _sprite.move(0, 2);
-    if (_sprite.getPosition().y >= 600) {
+    _sprite.move(0, 2); // Movimiento hacia abajo
+    if (_sprite.getPosition().y >= 600) { // Asegurando que no salga de la pantalla
         _sprite.setPosition(_sprite.getPosition().x, 600);
     }
 }
 
-void Banana::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    target.draw(_sprite, states);
-}
-
 void Banana::respawn() {
     _sprite.setPosition(std::rand() % 700 + _sprite.getGlobalBounds().width, std::rand() % 500 + _sprite.getGlobalBounds().width);
-}
-
-sf::FloatRect Banana::getBounds() const {
-    return _sprite.getGlobalBounds();
 }
--- Banana.h
+++ Banana.h
@@ -1,19 +1,9 @@
 #pragma once
-#include <SFML\Graphics.hpp>
-#include "Collisionable.h"
+#include "Fruta.h"
 
-class Banana: public sf::Drawable, public Collisionable //Sabe colisionar
-{
-    private:
-        sf::Sprite _sprite;
-        sf::Texture _texture;
-
-    public:
-        Banana();
-        void cmd();
-        void update();
-        void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-        void respawn(); //Reaparece el objeto en lugares random
-        sf::FloatRect getBounds() const override; //Sabe como es su rectangulo para colisionar
+class Banana : public Fruta {
+public:
+    Banana();
+    void update() override;
+    void respawn() override;
 };
-
