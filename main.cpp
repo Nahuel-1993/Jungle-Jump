4b029b573f9a887d1832886f740083c546b3cd11
Nahuel-1993 <nahuelfeijoo1993@gmail.com>
2024-11-04 19:50:36 -0300

    El gameloop esta en la funcion gameplay()

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "Camu.h"
 #include "Bacaba.h"
 #include "RespawnFruta.h"
+#include "Gameplay.h"
 #include <iostream>
 #include "rlutil.h" ///Librería para mejoreas del menú
 
@@ -88,56 +89,9 @@ int main()
     image.setTexture(tex);
 
 
-
     ///Game Loop (update del juego) *Se subdivide internamente*
 
-    while (window.isOpen())
-    {
-        /// 1° Read input - Actualiza los estados de los perifericos de entrada. ↓
-        /// Leer la cola de mensajes
-
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        /// 2° CMD (Leemos que se apreto y los ejecutamos)
-
-
-
-        alan.cmd(); //Evaluamos los comandos
-
-
-        /// 3° Update - Actualiza los estados del juego
-        ///(Se puede manejar junto con CMD pero Brian prefiere separarlos)
-
-
-        /// Actualizar los estados del juego
-        alan.update();
-
-        manejarRespawn(frutaActual, frutas, indiceFrutaActual, relojRespawn, enRespawn, puntos, sound, alan);
-
-        text.setString(std::to_string(puntos)); //Como los punto no son string, los convierte
-
-
-        window.clear(); //Borra la pantalla para que no se superpongan objetos
-
-        /// 4° DRAW (muestra en la pantalla lo que hace update)
-
-        ///Dibujamos el fondo
-        window.draw(image);
-
-        /// Dibujar personaje y fruta actual
-        window.draw(alan);
-        window.draw(*frutaActual);
-        window.draw(text);
-
-        /// 5° Display - Flip
-        window.display();
-
-    }
+    gameplay(window, alan, frutas, frutaActual, indiceFrutaActual, relojRespawn, enRespawn, puntos, sound, text, image);
 
     /// Liberacion del juego (Con SFML no hace falta)
 
